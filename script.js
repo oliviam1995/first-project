8873dc86ea60d110b3233fbb1ef1e68ee218dfcd
oliviam1995 <119007427+oliviam1995@users.noreply.github.com>
2023-04-08 19:28:34 +0100

    js practice

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const myElement = document.getElementsByClassName("bars");
-myElement( "bars" ).clone().appendTo( document.body );
-myElement( window ).scroll(function() {
-myElement( "span" ).css( "display", "inline" ).fadeOut( "slow" );
+jQuery(function($){
+    $(window).scroll(function() {
+  var $window = $(window),
+
+      $box2 = $("box2");
+      $bars = $("bars");
+
+      var scroll = $window.scrollTop() + ($window.height() / 2);
+
+      $box2.each(function () {
+        var $this = $(this);
+        if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+          $bars.addClass("imagewipe");
+        }
+      });
+    }).scroll();
 });
